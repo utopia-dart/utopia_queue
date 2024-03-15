f2f060093abc0c0ae1015eb7233ab5a500b7010a
Damodar Lohani <dlohani48@gmail.com>
2024-03-15 15:47:16 +0545

    fix connection api

--- example/utopia_queue_example_client.dart
+++ example/utopia_queue_example_client.dart
@@ -4,7 +4,7 @@ import 'dart:math';
 import 'package:utopia_queue/utopia_queue.dart';
 
 void main() async {
-  final connection = ConnectionRedis('localhost', 6379);
+  final connection = await ConnectionRedis.init('localhost', 6379);
   final client = Client(connection, queue: 'myqueue');
   await client
       .enqueue({'user': Random().nextInt(20), 'name': 'Damodar Lohani'});
